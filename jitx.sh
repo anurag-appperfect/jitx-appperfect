9aefd2d60432c89d96993fdd7760a370d507ebca
Anurag Mathur <94513745+anurag-appperfect@users.noreply.github.com>
2022-02-25 18:22:27 +0530

    Update jitx.sh

--- jitx.sh
+++ jitx.sh
@@ -27,43 +27,43 @@ fi
 if [ $# -eq 1 ]; then
     dir_path="$1""Exporter-QA"
     cd $dir_path
-    pytest test_exporter_jitx.py
+    pytest test_exporter_jitx.py --html=Exporter_report.html
     cd ../Importer-QA
-    pytest test_importer_jitx.py
+    pytest test_importer_jitx.py --html=Importer_report.html
     cd ../JITX-QA
-    pytest test_jitxqa_jitx.py
+    pytest test_jitxqa_jitx.py --html=JITX_report.html
     cd ../OCDB-QA
-    pytest test_ocdb_jitx.py
+    pytest test_ocdb_jitx.py --html=OCDB_report.html
     cd ../Roundtrip-QA
-    pytest test_roundtrip_jitx.py
+    pytest test_roundtrip_jitx.py --html=Roundtrip_report.html
     cd ../Schematic-QA
-    pytest test_schematic_jitx.py    
+    pytest test_schematic_jitx.py  --html=Schematic_report.html
 else
     option="${2}" 
     case ${option} in 
         "Exporter-QA") dir_path="$1""Exporter-QA" 
             cd $dir_path
-            pytest test_exporter_jitx.py
+            pytest test_exporter_jitx.py --html=Exporter_report.html
             ;; 
         "Importer-QA") dir_path="$1""Importer-QA" 
             cd $dir_path
-            pytest test_importer_jitx.py
+            pytest test_importer_jitx.py --html=Importer_report.html
             ;; 
         "JITX-QA") dir_path="$1""JITX-QA"
             cd $dir_path
-            pytest test_jitxqa_jitx.py
+            pytest test_jitxqa_jitx.py --html=JITX_report.html
             ;;
         "OCDB-QA") dir_path="$1""OCDB-QA"
             cd $dir_path
-            pytest test_ocdb_jitx.py
+            pytest test_ocdb_jitx.py --html=OCDB_report.html
             ;;
         "Roundtrip-QA") dir_path="$1""Roundtrip-QA"
             cd $dir_path
-            pytest test_roundtrip_jitx.py
+            pytest test_roundtrip_jitx.py --html=Roundtrip_report.html
             ;;
         "Schematic-QA") dir_path="$1""Schematic-QA"
             cd $dir_path
-            pytest test_schematic_jitx.py
+            pytest test_schematic_jitx.py --html=Schematic_report.html
             ;;
         *)
             echo "Invalid directory name"
